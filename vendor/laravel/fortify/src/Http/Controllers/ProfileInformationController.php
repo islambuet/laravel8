91efd7a586f994dd99724af78f299b43eb50080a
Md. Shaiful Islam <islambuet@gmail.com>
2020-11-15 03:19:42 +0600

    included files

---
+++ vendor/laravel/fortify/src/Http/Controllers/ProfileInformationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Laravel\Fortify\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
+
+class ProfileInformationController extends Controller
+{
+    /**
+     * Update the user's profile information.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Laravel\Fortify\Contracts\UpdatesUserProfileInformation  $updater
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,
+                           UpdatesUserProfileInformation $updater)
+    {
+        die('ere');
+        $updater->update($request->user(), $request->all());
+
+        return $request->wantsJson()
+                    ? new JsonResponse('', 200)
+                    : back()->with('status', 'profile-information-updated');
+    }
+}
